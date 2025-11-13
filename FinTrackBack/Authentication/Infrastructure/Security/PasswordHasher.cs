881fb2852f7e9c460bc0aa471a19a4be9c9c8eb4
Fabrizio Quiroz <u202213406@upc.edu.pe>
2025-11-13 10:51:01 -0500

    Update PasswordHasher.cs

--- FinTrackBack/Authentication/Infrastructure/Security/PasswordHasher.cs
+++ FinTrackBack/Authentication/Infrastructure/Security/PasswordHasher.cs
@@ -9,10 +9,8 @@ public class PasswordHasher : IPasswordHasher
         return BCrypt.Net.BCrypt.HashPassword(password);
     }
 
-    // V-- AÑADE ESTE MÉTODO --V
     public bool VerifyPassword(string password, string passwordHash)
     {
-        // BCrypt se encarga de comparar el texto plano con el hash
         return BCrypt.Net.BCrypt.Verify(password, passwordHash);
     }
 }
